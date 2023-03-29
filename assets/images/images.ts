080534c6de6ee276aeabbf59af781c2d3bfa4a0a
Alex Blokh <aleksandrblokh@gmail.com>
2023-03-29 16:49:26 +0300

    icons

--- assets/images/images.ts
+++ assets/images/images.ts
@@ -1,6 +1,10 @@
 import neon from './neon-serverless.svg';
 import postgresjslight from './postgres-js-light.svg';
 import postgresjsdark from './postgres-js-dark.svg';
+import dbdark from "./database-dark.svg"
+import dblight from "./database-light.svg"
+import serverlight from "./server-light.svg"
+import serverdark from "./server-dark.svg"
 
 const images: {
   [key: string]: {
@@ -10,6 +14,10 @@ const images: {
   'neon-serverless': neon,
   'postgres-js-dark': postgresjsdark,
   'postgres-js-light': postgresjslight,
+  'database-light': dblight,
+  'database-dark': dbdark,
+  'server-dark': serverdark,
+  'server-light': serverlight,
 };
 
 export default images;
