57730abafe4e999799f459ed04a0f0ed7a9cae6f
vlad-stohnii <vladislav.stohnii@gmail.com>
2023-03-29 18:19:57 +0300

    card theme changes

--- assets/images/images.ts
+++ assets/images/images.ts
@@ -1,17 +1,19 @@
-import neon from './neon-serverless.svg';
+import neonlight from './neon-serverless-light.svg';
+import neondark from './neon-serverless-dark.svg';
 import postgresjslight from './postgres-js-light.svg';
 import postgresjsdark from './postgres-js-dark.svg';
-import dbdark from "./database-dark.svg"
-import dblight from "./database-light.svg"
-import serverlight from "./server-light.svg"
-import serverdark from "./server-dark.svg"
+import dbdark from './database-dark.svg';
+import dblight from './database-light.svg';
+import serverlight from './server-light.svg';
+import serverdark from './server-dark.svg';
 
 const images: {
   [key: string]: {
     src: string;
   }
 } = {
-  'neon-serverless': neon,
+  'neon-serverless-dark': neondark,
+  'neon-serverless-light': neonlight,
   'postgres-js-dark': postgresjsdark,
   'postgres-js-light': postgresjslight,
   'database-light': dblight,
