8f7b65f2891210647d2f08abb0308384fd6c630f
vlad-stohnii <vladislav.stohnii@gmail.com>
2023-04-22 21:01:55 +0300

    fix: ts call stack

--- components/pages/SQLPlayground/libCreator.ts
+++ components/pages/SQLPlayground/libCreator.ts
@@ -2,17 +2,19 @@ import globals from './globals';
 import bdWithImports from './bdWithImports';
 
 const libCreator = async (dbname: string) => {
-  const modulesNew = await Promise.all(bdWithImports[dbname].types.map(async (module) => {
+  const strings = await Promise.all(bdWithImports[dbname].types.map(async (module) => {
     const f = () => import(`!!raw-loader!../../../types/${module}/index.d.ts`);
     const p = await f();
-    return ({
-      content: `import * from 'drizzle-orm';
-  declare global {
-    ${p.default}
-  }`,
-      filePath: `file:///node_modules/${module}/index.d.ts`,
-    });
+    return (p.default);
   }));
+  const modulesNew = [{
+    content: `import * from 'drizzle-orm';
+      declare global {
+          ${strings.join(' ')};
+        },
+    `,
+    filePath: 'drizzle-orm.d.ts',
+  }];
   modulesNew.push({
     content: globals[dbname],
     filePath: 'file:///global.d.ts',
@@ -21,22 +23,3 @@ const libCreator = async (dbname: string) => {
 };
 
 export default libCreator;
-// const libCreator = async (dbname: string) => {
-//   const modulesNew = await Promise.all(modules.map(async (module) => {
-//     const f = () => import(`!!raw-loader!../../../types/${module}/index.d.ts`);
-//     const p = await f();
-//     return ({
-//       content: `declare module '${module}' {
-//         ${p.default}
-//       }`,
-//       filePath: `file:///node_modules/${module}/index.d.ts`,
-//     });
-//   }));
-//   modulesNew.push({
-//     content: globals[dbname],
-//     filePath: 'file:///global.d.ts',
-//   });
-//   return modulesNew;
-// };
-
-// export default libCreator;
