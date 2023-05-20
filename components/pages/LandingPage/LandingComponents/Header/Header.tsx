4202bafbe10dded98cc5d3e40cd0e3312afad0e5
Dan Kochetov <danil.kochetov@gmail.com>
2023-05-20 17:14:32 +0200

    Add interactive team table

--- components/pages/LandingPage/LandingComponents/Header/Header.tsx
+++ components/pages/LandingPage/LandingComponents/Header/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Typer from './Typer/Typer';
 import styles from './Header.module.css';
 import MainPic from './Images/MainPic.png';
-import MainPicDark from './Images/MainPic_dark.png';
 import SecondPic from './Images/SecondPic.png';
 import SecondPicDark from './Images/SecondPic_dark.png';
 import ParticlePic from './Images/ParticlePic.png';
@@ -26,7 +25,45 @@ const Header = () => {
         <div className={styles.picture_block}>
           <img className={styles.addition_image} src={isLight ? SecondPic.src : SecondPicDark.src} alt="Second" />
           <div className={styles.mainpicture_wrapper}>
-            <img className={styles.picture} src={isLight ? MainPic.src : MainPicDark.src} alt="Main" />
+            <img className={styles.picture} src={MainPic.src} alt="Main" />
+            <div className={styles.team}>
+              <div className={styles.id}>id</div>
+              <div>name</div>
+              <div>twitter_handle</div>
+              <div className={styles.id}>1</div>
+              <div>Dan</div>
+              <a
+                className={styles.handle}
+                href="https://twitter.com/bloberenober"
+                target="_blank"
+                data-text="@bloberenober"
+                rel="noreferrer"
+              >
+                @bloberenober
+              </a>
+              <div className={styles.id}>2</div>
+              <div>Andrew</div>
+              <a
+                className={styles.handle}
+                href="https://twitter.com/andrii_sherman"
+                target="_blank"
+                data-text="@andrii_sherman"
+                rel="noreferrer"
+              >
+                @andrii_sherman
+              </a>
+              <div className={styles.id}>3</div>
+              <div>Alex</div>
+              <a
+                className={styles.handle}
+                href="https://twitter.com/_alexblokh"
+                target="_blank"
+                data-text="@_alexblokh"
+                rel="noreferrer"
+              >
+                @_alexblokh
+              </a>
+            </div>
             <img className={styles.particle} src={isLight ? ParticlePic.src : ParticlePicDark.src} alt="Particle" />
           </div>
         </div>
