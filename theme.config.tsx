d4c6d1972b5af3ce130e084438409409892b22dd
Vlad Stohnii <vladislav.stohnii@gmail.com>
2023-05-26 11:36:34 +0300

    added favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -60,6 +60,11 @@ const config: DocsThemeConfig = {
   editLink: {
     text: '',
   },
+  head: (
+    <>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    </>
+  ),
   footer: {
     component: () => <div />,
   },
