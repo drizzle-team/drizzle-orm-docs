06329af51ea4e07c2bd237523641684d6429dc69
Vlad Stohnii <vladislav.stohnii@gmail.com>
2023-06-02 18:47:20 +0300

    +

--- components/pages/LandingPage/LandingComponents/Pricing/Pricing.tsx
+++ components/pages/LandingPage/LandingComponents/Pricing/Pricing.tsx
@@ -1,51 +1,75 @@
+import { StaticImageData } from 'next/image';
+import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
 import styles from './Pricing.module.css';
 import {
   DiscordIcon, GiftIcon, StarIcon, TwitterIcon,
 } from '../../../../Icons/Icons';
-import Image from '../Supporting/Images/LiveOnTheEdgeDark.png';
+import MoneyImage from './Images/Money.png';
+import MoneyImageDark from './Images/MoneyDark.png';
+import WithoutMoneyImage from './Images/WithoutMoney.png';
+import WithoutMoneyImageDark from './Images/WithoutMoneyDark.png';
 
-const Pricing = () => (
-  <div className={styles.wrap}>
-    <div className={styles.title}>Pricing</div>
-    <div className={styles.description}>
-      Omg, just joking, Drizzle is free and open-source ofc
-      <br />
-      Yet you can contribute!
-    </div>
-    <div className={styles.blocks_wrap}>
-      <div className={styles.block}>
-        <div className={styles.image_wrap}>
-          <img src={Image.src} alt="test" />
+const Pricing = () => {
+  const { theme } = useTheme();
+  const [isLight, setIsLight] = useState(false);
+  useEffect(() => {
+    setIsLight(theme === 'light');
+  }, [theme]);
+  const images: {
+    [key: string]: StaticImageData;
+  } = {
+    withoutMoney: isLight ? WithoutMoneyImage : WithoutMoneyImageDark,
+    money: isLight ? MoneyImage : MoneyImageDark,
+  };
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.title}>Pricing</div>
+      <div className={styles.description}>
+        Omg, just joking, Drizzle is free
+        <span className={styles.break}>
+          <br />
+        </span>
+        and open-source ofc
+        <br />
+        You can still make your contribution!
+      </div>
+      <div className={styles.blocks_wrap}>
+        <div className={styles.left_block}>
+          <div className={styles.image_wrap}>
+            <img src={images.withoutMoney.src} alt="test" />
+          </div>
+          <div className={styles.buttons_wrap}>
+            <a className={`${styles.button} ${styles.github}`} href="https://github.com/drizzle-team/drizzle-orm" target="_blank" rel="noreferrer nofollow">
+              <StarIcon />
+              Give us a star
+            </a>
+            <a className={`${styles.button} ${styles.twitter}`} target="_blank" href="https://mobile.twitter.com/DrizzleOrm" aria-label="Drizzle twitter" rel="nofollow noreferrer">
+              <TwitterIcon />
+              Subscribe on Twitter
+            </a>
+            <a className={`${styles.button} ${styles.discord}`} href="https://discord.gg/yfjTbVXMW4" target="_blank" rel="noreferrer">
+              <DiscordIcon />
+              Join our community
+            </a>
+          </div>
         </div>
-        <div className={styles.buttons_wrap}>
-          <a className={`${styles.button} ${styles.github}`} href="https://github.com/drizzle-team/drizzle-orm" target="_blank" rel="noreferrer nofollow">
-            <StarIcon />
-            Give us a star
-          </a>
-          <a className={`${styles.button} ${styles.twitter}`} target="_blank" href="https://mobile.twitter.com/DrizzleOrm" aria-label="Drizzle twitter" rel="nofollow noreferrer">
-            <TwitterIcon />
-            Subscribe on Twitter
-          </a>
-          <a className={`${styles.button} ${styles.discord}`} href="https://discord.gg/yfjTbVXMW4" target="_blank" rel="noreferrer">
-            <DiscordIcon />
-            Join our community
-          </a>
+        <div className={styles.divider} />
+        <div className={styles.right_block}>
+          <div className={styles.buttons_wrap_sponsor}>
+            <p className={styles.buttons_description}>Buy us some ramen 🍜</p>
+            <a className={`${styles.button}  ${styles.sponsor_button}`} href="https://github.com/sponsors/drizzle-team" rel="nofollow noreferrer" target="_blank">
+              <GiftIcon />
+              Become a sponsor
+            </a>
+          </div>
+          <div className={styles.image_wrap}>
+            <img src={images.money.src} alt="test" />
+          </div>
         </div>
       </div>
-      <div className={styles.block}>
-        <div className={styles.image_wrap}>
-          <img src={Image.src} alt="test" />
-        </div>
-        <div className={`${styles.buttons_wrap}`}>
-          <a className={`${styles.button}  ${styles.sponsor_button}`} href="https://github.com/sponsors/drizzle-team" rel="nofollow noreferrer" target="_blank">
-            <GiftIcon />
-            Become a sponsor
-          </a>
-        </div>
-      </div>
     </div>
-  </div>
-
-);
+  );
+};
 
 export default Pricing;
