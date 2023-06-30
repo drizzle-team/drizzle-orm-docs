f751087a14c5e0aee10683afc14ad6456f72493f
kyrylolvov <kyrylolvov@icloud.com>
2023-06-30 12:18:43 -0400

    CHANGED: - Updated scrop of drizzle studio screenshots - Updated layout of Drizzle Studio block - Updated styling, added accent button

--- components/pages/LandingPage/LandingComponents/DrizzleCloud/DrizzleStudio.tsx
+++ components/pages/LandingPage/LandingComponents/DrizzleCloud/DrizzleStudio.tsx
@@ -6,25 +6,24 @@ import useGetTheme from '../../../../hooks/useGetTheme';
 const DrizzleStudio = () => {
   const isLight = useGetTheme();
   return (
-    <div className={styles.wrap}>
-      <div className={styles.title}>Drizzle Studio</div>
-      <div className={styles.buttons}>
-        <a className={styles.button} href="https://github.com/drizzle-team/drizzle-orm">
-          Live Demo
-        </a>
+    <div className={styles.container}>
+      <div className={styles.text_container}>
+        <div className={styles.title}>Drizzle Studio</div>
+        <div className={styles.description}>
+          The easiest way to explore and
+          manipulate your data in all of your Drizzle projects.
+        </div>
       </div>
-      <div>
+      <div className={styles.image_container}>
         <img className={styles.image} src={!isLight ? DrizzleImgDark.src : DrizzleImgLight.src} alt="Drizzle Studio" />
       </div>
-      <div className={styles.docs}>
-        The easiest way to explore and
-        manipulate your data in all of your Drizzle projects.
-        <span className={styles.link}>
-          <a href="https://github.com/drizzle-team/drizzle-orm">
-            Head over to the docs
-          </a>
-        </span>
-        to learn more!
+      <div className={styles.buttons}>
+        <a className={styles.button_accent} href="https://github.com/drizzle-team/drizzle-orm">
+          Live Demo
+        </a>
+        <a className={styles.button} href="https://github.com/drizzle-team/drizzle-orm">
+          Documentation
+        </a>
       </div>
     </div>
   );
