48f105b5c0628a2bdaec0ecb44274f8351e65f7a
Vlad Stohnii <vladislav.stohnii@gmail.com>
2023-06-30 17:23:35 +0300

    +

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
-import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-import 'simplebar/dist/simplebar.css';
-import 'monaco-editor/esm/vs/base/browser/ui/actionbar/actionbar.css';
+import "simplebar"; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import "simplebar/dist/simplebar.css";
 
-import './style.css'
+import "./style.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
