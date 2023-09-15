b4102a94baf7d6aca8ee0a0df7ff90b0d180a978
Vlad Stohnii <vladislav.stohnii@gmail.com>
2023-09-15 15:28:38 +0300

    fix: design issues

--- components/Navigation/components/MobileNav/MobileNav.tsx
+++ components/Navigation/components/MobileNav/MobileNav.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
 import styles from './MobileNav.module.css';
 import MobileNavItem from './MobileNavItem/MobileNavItem';
+import DiscordLink from '@/components/Icons/DiscordLink';
+import GithubLink from '@/components/Icons/GithubLink';
+import TwitterLink from '@/components/Icons/TwitterLink';
 
 interface Props {
   isOpened: boolean;
@@ -12,6 +15,11 @@ const MobileNav :FC<Props> = ({ isOpened, setIsOpened }) => (
       <MobileNavItem setIsOpened={setIsOpened} value="Drizzle ORM" href="/docs/overview" activeKeywords={['/docs']} />
       <MobileNavItem setIsOpened={setIsOpened} value="Drizzle Kit" href="/kit-docs/overview" activeKeywords={['/kit-docs']} />
       <MobileNavItem setIsOpened={setIsOpened} value="Drizzle Studio" href="/drizzle-studio/overview" activeKeywords={['/drizzle-studio']} />
+    </div>
+    <div className={styles.nav_socials}>
+      <GithubLink />
+      <DiscordLink />
+      <TwitterLink />
     </div>
   </div>
 );
