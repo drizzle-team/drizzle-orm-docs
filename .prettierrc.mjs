a6acc995f8588efa1d55ab4bd064886992295bbf
Vlad Stohnii <vladislav.stohnii@gmail.com>
2023-12-08 14:08:41 +0200

    +

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
---
+++ src/data/asideSponsors.ts
@@ -0,0 +1,41 @@
+export interface IAsideSponsor {
+  name: string;
+  href: string;
+  imgSrc: string;
+}
+
+export const asideSponsors: IAsideSponsor[][] = [
+  [
+    {
+      name: "Neon",
+      href: "https://driz.link/neon",
+      imgSrc: "/images/aside-sponsors/neon-logo.svg",
+    },
+  ],
+  [
+    {
+      name: "Xata",
+      href: "https://driz.link/xata",
+      imgSrc: "/images/aside-sponsors/xata-logo.svg",
+    },
+  ],
+  [
+    {
+      name: "Turso",
+      href: "https://driz.link/turso",
+      imgSrc: "/images/aside-sponsors/turso-logo.svg",
+    },
+  ],
+  [
+    {
+      name: "Neon",
+      href: "https://driz.link/neon",
+      imgSrc: "/images/aside-sponsors/neon-logo.svg",
+    },
+    {
+      name: "Xata",
+      href: "https://driz.link/xata",
+      imgSrc: "/images/aside-sponsors/xata-logo.svg",
+    },
+  ],
+];
