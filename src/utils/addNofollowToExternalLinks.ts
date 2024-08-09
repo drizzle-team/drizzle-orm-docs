63c4b0b8d740a7ecb2d3662f7e5fd63e874d537d
SvmurviWolf <defors3@gmail.com>
2024-08-09 16:35:05 +0300

    ADDED: target blank to links in shipping

--- src/utils/addNofollowToExternalLinks.ts
+++ src/utils/addNofollowToExternalLinks.ts
@@ -1,5 +1,12 @@
 export default (html: string): string => {
+  // Добавляем rel="nofollow" и target="_blank" для внешних ссылок, если их нет
   const externalLinkPattern =
     /<a\s+(?![^>]*\brel=["']nofollow["'])([^>]*\bhref=["']https?:\/\/(?!(orm\.drizzle\.team|drizzle\.team)[^"']*)[^"']+["'][^>]*)>/gi;
-  return html.replace(externalLinkPattern, '<a $1 rel="nofollow">');
+
+  return (
+    html
+      .replace(externalLinkPattern, '<a $1 rel="nofollow">')
+      // Добавляем target="_blank" для всех ссылок, если его нет
+      .replace(/<a(?![^>]*\btarget=["'][^"']*["'])/gi, '<a target="_blank"')
+  );
 };
