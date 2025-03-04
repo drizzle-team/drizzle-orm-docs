62fd1108cdd7a8931a5d86deda1ada2c7abc21e3
Vlad Stohnii <vladislav.stohnii@gmail.com>
2025-03-04 17:16:49 +0200

    feat: llms generation

--- src/pages/llms-full.txt.ts
+++ src/pages/llms-full.txt.ts
@@ -1,8 +1,30 @@
-import { description, generateFileContent, getAllFiles, title, } from '@/data/llms';
-import type { APIRoute } from 'astro';
+import { description, title } from "@/data/llms";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
 
-export const GET: APIRoute = async () => {
-  const files = await getAllFiles();
-  const contents = await Promise.all(files.map((file) => generateFileContent(file)));
-  return new Response(`# ${title}\n\n${description}${contents.join('')}`);
-}
+export const GET: APIRoute = async ({ url }) => {
+  let llms = `# ${title}\n\n> ${description}\n\n`;
+
+  const docCollection = await getCollection("docs", (entry) => {
+    return {
+      slug: entry.slug,
+      body: entry.body,
+    }
+  });
+
+  docCollection.filter((entry) => {
+    if (entry.slug.includes("latest-releases") || entry.slug.includes("migrate/")) {
+      return false;
+    }
+    return true;
+  }).forEach((doc) => {
+    if (doc.slug.includes("tutorials/")) {
+      const tutorialSlug = doc.slug.split("/").at(-1);
+      llms += `Source: ${url.origin}/docs/tutorials/${tutorialSlug}\n\n${doc.body}\n\n`;
+    } else {
+      llms += `Source: ${url.origin}/docs/${doc.slug}\n\n${doc.body}\n\n`;
+    }
+  });
+
+  return new Response(llms);
+};
--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -1,47 +1,159 @@
 // https://llmstxthub.com/guides/getting-started-llms-txt
-import { description, generateSection, llmsFiles, title, type LlmsFile } from '@/data/llms';
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+import {
+  description,
+  title,
+} from "@/data/llms";
 
 export const GET: APIRoute = async ({ url }) => {
-  let llms = `# ${title}\n\n> ${description}`;
-  let files: LlmsFile[] = [];
+  // Get the URL
+  const getUrl = (path: string) => `${url.origin}/docs/${path}`;
 
-  files = await llmsFiles.getStarted();
-  llms += await generateSection(url, 'Getting started', files, '/docs/get-started', 'content');
+  // Create the LLMS
+  let llms = `# ${title}\n\n> ${description}\n`;
 
-  files = await llmsFiles.guides();
-  llms += await generateSection(url, 'Guides', files, '/docs/guides', 'frontmatter');
+  const docCollection = await getCollection("docs", (entry) => {
+    return {
+      slug: entry.slug,
+      body: entry.body,
+      data: entry.data,
+    };
+  });
 
-  files = await llmsFiles.tutorials();
-  llms += await generateSection(url, 'Tutorials', files, '/docs/tutorials', 'frontmatter');
+  // Get all meta files
+  const metaFiles = import.meta.glob("../content/**/_meta.json");
 
-  files = llmsFiles.fundamentals();
-  llms += await generateSection(url, 'Fundamentals', files, '/docs', 'content');
+  // Get all slugs from meta files
+  const metaFilesSlugs = await Promise.all(
+    Object.keys(metaFiles).map(async (meta) => {
+      const metaFilePath = meta.replace("/_meta.json", "");
+      const directoryName = metaFilePath.split("/")[3];
+      // Add directory name as prefix to the slug
+      const slugPrefix = directoryName ? `${directoryName}/` : "";
 
-  files = llmsFiles.connect();
-  llms += await generateSection(url, 'Connect to a database', files, '/docs', 'content');
+      const { default: parsed } = (await metaFiles[meta]()) as {
+        default: string[] | string[][];
+      };
+      return parsed.map((slug) => {
+        if (Array.isArray(slug)) {
+          return slug.map((subSlug) => `${slugPrefix}${subSlug}`);
+        }
+        return `${slugPrefix}${slug}`;
+      });
+    }),
+  );
 
-  files = await llmsFiles.schema.types();
-  llms += await generateSection(url, 'Manage schema', files, '/docs/column-types', 'frontmatter');
-  files = llmsFiles.schema.other();
-  llms += await generateSection(url, null, files, '/docs', 'content');
-  files = await llmsFiles.schema.extensions();
-  llms += await generateSection(url, null, files, '/docs/extensions', 'frontmatter');
+  // Remove slugs with "---" and replace "::"(collapse block)
+  const slugs = metaFilesSlugs
+    .flat()
+    .filter((slug) => !slug.includes("---"))
+    .map((slug) => (typeof slug === "string" ? slug.replace("::", "") : slug));
 
-  files = llmsFiles.migrations();
-  llms += await generateSection(url, 'Migrations', files, '/docs', 'content');
+  // Reorder the array (grouped by prefix)
+  const reorderArray = (arr: string[]) => {
+    const grouped: Record<string, string[]> = {};
+    const order: string[] = [];
 
-  files = llmsFiles.seeding();
-  llms += await generateSection(url, 'Seeding', files, '/docs', 'content');
+    arr.forEach((item) => {
+      const prefix = item.includes("/") ? item.split("/")[0] : item;
+      if (!grouped[prefix]) {
+        grouped[prefix] = [];
+        order.push(prefix);
+      }
+      grouped[prefix].push(item);
+    });
 
-  files = llmsFiles.data();
-  llms += await generateSection(url, 'Access your data', files, '/docs', 'content');
+    return order.flatMap((prefix) => grouped[prefix]);
+  }
 
-  files = llmsFiles.performance();
-  llms += await generateSection(url, 'Performance', files, '/docs', 'content');
+  // Get slugs without array
+  const slugsWithOneValue = slugs.map((slug) => {
+    if (Array.isArray(slug)) {
+      return slug[0];
+    }
+    return slug;
+  });
 
-  files = llmsFiles.extensions();
-  llms += await generateSection(url, 'Extensions, integrations and plugins', files, '/docs', 'content');
+  const sortedSlugs = reorderArray(slugsWithOneValue);
+
+  const formatedSlugs = sortedSlugs.map((slug) => {
+    const slugArray = slugs.find((s) => s[0] === slug) as string[] | undefined;
+    if (slugArray) {
+      return slugArray;
+    }
+    return slug;
+  });
+
+  // Add the slugs to the LLMS
+  formatedSlugs.forEach((slug) => {
+    if (typeof slug === "string") {
+      llms += `\n## ${slug}\n\n`;
+    }
+
+    // if the slug is an array, it means its a section
+    if (Array.isArray(slug)) {
+      const collectionEntry = docCollection.find(
+        (entry) => slug[0] === entry.slug,
+      );
+      // Find title in the body
+      const findTitle = collectionEntry?.body.match(/^# (.+)/gm);
+      if (findTitle) {
+        const sectionTitle = findTitle[0].replace("# ", "");
+        llms += `- [${sectionTitle}](${getUrl(slug[0])})\n`;
+      } else {
+        // If title is not found in the body, use the title from the meta file
+        const entryTitle = collectionEntry?.data.title;
+        if (entryTitle) {
+          llms += `- [${entryTitle}](${getUrl(slug[0])})\n`;
+        }
+      }
+    }
+  });
+
+  // Guides
+  const mapFiles = import.meta.glob("../content/**/_map.json");
+  const guidesSlugs = [];
+  for (let map in mapFiles) {
+    const { default: parsed } = (await mapFiles[map]()) as {
+      default: string[][];
+    };
+    // Add the parsed slugs to the guidesSlugs array
+    guidesSlugs.push(...parsed);
+  }
+
+  llms += `\n## Guides\n\n`;
+
+  // Main Guides page
+  llms += `- [Guides](${getUrl("guides")})\n`;
+  guidesSlugs.forEach((slug) => {
+    const collectionEntry = docCollection.find(
+      (entry) => slug[0] === entry.slug,
+    );
+    const guideTitle = collectionEntry?.data.title;
+    const guideSlug = collectionEntry?.data.slug || slug[0];
+    if (guideTitle) {
+      llms += `- [${guideTitle}](` + getUrl(`guides/${guideSlug}`) + `)\n`;
+    }
+  });
+
+  // Tutorials
+  llms += `\n## Tutorials\n\n`;
+
+  const tutorialsEntries = docCollection.filter((entry) => entry.slug.includes("tutorials"));
+
+  // Main Tutorials page
+  llms += `- [Tutorials](${getUrl("tutorials")})\n`;
+
+  tutorialsEntries.forEach((entry) => {
+    const tutorialTitle = entry.data.title;
+    // Tutorial slug has group name as prefix, so we need to remove it to get the correct slug
+    const tutorialsSlug = "tutorials/" + entry.slug.split("/").at(-1);
+    if (tutorialTitle) {
+      llms += `- [${tutorialTitle}](` + getUrl(tutorialsSlug) + `)\n`;
+    }
+  });
 
   return new Response(llms);
 };
