59b8884613f7c1b7db843d8b7b508c1ddd401dcc
AndriiSherman <andreysherman11@gmail.com>
2026-01-07 10:19:50 +0200

    Add effect to docs

--- src/mdx/GetStartedLinks/data.ts
+++ src/mdx/GetStartedLinks/data.ts
@@ -187,6 +187,28 @@ export const getStartedItems: GetStartedItems = [
           },
         },
       },
+      {
+        name: "Effect",
+        path: {
+          existing: "/docs/get-started/effect-postgresql-existing",
+          new: "/docs/get-started/effect-postgresql-new",
+        },
+        icon: {
+          light: {
+            path: "/public/svg/effect_light.svg",
+            style: {
+              width: 20,
+            },
+          },
+          dark: {
+            path: "/public/svg/effect.svg",
+            style: {
+              width: 20,
+              fill: "#f0f0f0",
+            },
+          },
+        },
+      },
     ],
   },
   {
