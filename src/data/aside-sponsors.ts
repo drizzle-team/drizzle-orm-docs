044fa277792e1dd7d64cb754db7170d648ad0295
AndriiSherman <andreysherman11@gmail.com>
2026-01-10 12:06:06 +0200

    +

--- src/data/aside-sponsors.ts
+++ src/data/aside-sponsors.ts
@@ -310,8 +310,7 @@ export const asideSponsors: ISponsor[] = [
       __typename: "Organization",
       login: "driz.link/replit",
       name: "Replit",
-      avatarUrl:
-        `<svg version="1.1" id="Artwork" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      avatarUrl: `<svg version="1.1" id="Artwork" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 793.8 871.5" style="enable-background:new 0 0 793.8 871.5;" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#F5640C;}
